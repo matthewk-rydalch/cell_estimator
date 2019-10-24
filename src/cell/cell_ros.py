c2ec4422157bf2efd0a803df60c194861e171c2b
Matt Rydalch <matthewk.rydalch@gmail.com>
2019-10-24 17:19:47 -0600

    working on getting messages to work

--- src/cell/cell_ros.py
+++ src/cell/cell_ros.py
@@ -11,39 +11,16 @@ def cell_ros():
 	# rospy.Subscriber('cell/imu', cell_imu, self.cellImuCallback)
 	# rospy.Subscriber('cell/RelPosNED', cell_RelPosNED, self.cellRelposnedCallback)
 	
-	pub = rospy.Publisher('cell/imu', imu, queue_size=10) #can include a queue size.  See ros docs for publishers
+	pub = rospy.Publisher('cell/imu', imu, queue_size=1024) #can include a queue size.  See ros docs for publishers
 	rospy.init_node('cell_ros', anonymous=True)
 	rate = rospy.Rate(10) # 10hz
 	while not rospy.is_shutdown():
 
-		std_msgs/Header header
-  uint32 seq
-  time stamp
-  string frame_id
-geometry_msgs/Quaternion orientation
-  float64 x
-  float64 y
-  float64 z
-  float64 w
-float64[9] orientation_covariance
-geometry_msgs/Vector3 angular_velocity
-  float64 x
-  float64 y
-  float64 z
-float64[9] angular_velocity_covariance
-geometry_msgs/Vector3 linear_acceleration
-  float64 x
-  float64 y
-  float64 z
-float64[9] linear_acceleration_covariance
-		orientation = imu.orientation()
-		orientation.x = 1
-		orientation.y = 2
-		orientation.z = 3
-		orientation.w = 4
-		
-		imu_new.orientation
+		imu_data = imu()
 
+		imu_data.var = 5.0
+
+		pub.publish(imu_data)
 
 		rate.sleep()
 		rospy.spin()
