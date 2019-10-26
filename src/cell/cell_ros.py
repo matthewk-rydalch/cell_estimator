9111036717e67016303ad23dad8f2a9442dd784c
Matt Rydalch <matthewk.rydalch@gmail.com>
2019-10-26 17:58:41 -0600

    I got all the custom messages created and publishing now.

--- src/cell/cell_ros.py
+++ src/cell/cell_ros.py
@@ -4,10 +4,15 @@ from IPython.core.debugger import set_trace
 import rospy
 from std_msgs.msg import String
 from cell_estimator.msg import imu
+from cell_estimator.msg import RelPos
+from cell_estimator.msg import PositionVelocityTime
 
 def cell_ros():
 
-	pub = rospy.Publisher('cell/imu', imu, queue_size=1024) #can include a queue size.  See ros docs for publishers
+	pub_imu = rospy.Publisher('cell/imu', imu, queue_size=1024) #I just used the queue size from latis
+	pub_NED = rospy.Publisher('cell/NED', RelPos, queue_size=1024) 
+	pub_lla = rospy.Publisher('cell/lla', PositionVelocityTime, queue_size=1024) 
+
 	rospy.init_node('cell_ros', anonymous=True)
 
 	#example of how to set up a subscriber can be seen in estimator_ros.py
@@ -15,6 +20,8 @@ def cell_ros():
 	while not rospy.is_shutdown():
 
 		imu_data = imu()
+		NED_data = RelPos()
+		lla_data = PositionVelocityTime()
 
 		#quaternian
 		imu_data.orientation.x = 1.0
@@ -36,7 +43,9 @@ def cell_ros():
 
 		imu_data.linear_acceleration_covariance = [4.0, 5.0, 5.0, 5.0, 5.0, 5.0, 9.0, 0.0, 1.0]
 
-		pub.publish(imu_data)
+		pub_imu.publish(imu_data)
+		pub_NED.publish(NED_data)
+		pub_lla.publish(lla_data)
 
 if __name__ == '__main__':
 	cell_ros()
