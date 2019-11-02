4144a2c262247a2a9591b01f916266906d847a97
Matt Rydalch <matthewk.rydalch@gmail.com>
2019-11-02 15:03:01 -0600

    got UBLOX read subscriptions.  Worked with namespaces and there may be some issues in the future.  Possibly need to remove namespaces from anything that is subscribing.

--- src/estimator/estimator.py
+++ src/estimator/estimator.py
@@ -4,12 +4,19 @@ class Estimator():
     def imu_callback(self, data):
         #this line is simply to verify that messages are being subscribed to during development
         # rospy.loginfo(rospy.get_caller_id() + " orientation x %s", data.orientation.x)
-        print("imu_callback \n")
+        # print("imu_callback \n")
+        a=1
 
     def ned_callback(self, data):
         #this line is simply to verify that messages are being subscribed to during development
-        print("ned_callback \n")
+        # print("ned_callback \n")
+        a=1
 
     def lla_callback(self, data):
         #this line is simply to verify that messages are being subscribed to during development
-        print("lla_callback \n")
+        # print("lla_callback \n")
+        a=1
+        
+    def rover_RelPos_callback(self, data):
+        #this line is simply to verify that messages are being subscribed to during development
+        print("rover_RelPos_callback \n")
--- src/estimator/estimator_ros.py
+++ src/estimator/estimator_ros.py
@@ -5,13 +5,16 @@ from std_msgs.msg import String
 from cell_estimator.msg import imu
 from cell_estimator.msg import RelPos
 from cell_estimator.msg import PositionVelocityTime
+from ublox.msg import PositionVelocityTime
+from ublox.msg import RelPos
 from estimator import Estimator
 
 def ros_estimator():
 	rospy.init_node('estimator', anonymous=True)
-	rospy.Subscriber('imu', imu, est.imu_callback)
-	rospy.Subscriber('NED', RelPos, est.ned_callback)
-	rospy.Subscriber('lla', PositionVelocityTime, est.lla_callback)
+	rospy.Subscriber('cell/imu', imu, est.imu_callback)
+	rospy.Subscriber('cell/NED', RelPos, est.ned_callback)
+	rospy.Subscriber('cell/lla', PositionVelocityTime, est.lla_callback)
+	rospy.Subscriber('rover/RelPos', RelPos, est.rover_RelPos_callback)
 
 	rospy.spin()
 #
