771e1c4aa6b99bd79cbd50e4fdd9b393f4bfd56a
Michael Eyler <eyler94@gmail.com>
2019-11-05 09:17:00 -0700

    First stab at cell_ros is done. Everything should be good, NED converter will probably need some work once we can combine it with the base station lla.

--- src/estimator/estimator.py
+++ src/estimator/estimator.py
@@ -4,12 +4,13 @@ class Estimator():
     def imu_callback(self, data):
         #this line is simply to verify that messages are being subscribed to during development
         # rospy.loginfo(rospy.get_caller_id() + " orientation x %s", data.orientation.x)
-        print("imu_callback \n")
+        print("imu linear_acceleration:", data.linear_acceleration)
+        print("imu angular_velocity:", data.angular_velocity)
 
     def ned_callback(self, data):
         #this line is simply to verify that messages are being subscribed to during development
-        print("ned_callback \n")
+        print("ned data:")#, data)
 
     def lla_callback(self, data):
         #this line is simply to verify that messages are being subscribed to during development
-        print("lla_callback \n")
+        print("lla data:", data.lla)
