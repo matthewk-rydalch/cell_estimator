68b3184fe8c67da703533b0e718948ad9f32fbd5
Matt Rydalch <matthewk.rydalch@gmail.com>
2019-12-13 13:12:48 -0700

    About to do another test to try and get cleaner acceleration data and no backing up before we bag.

--- src/estimator/ekf.py
+++ src/estimator/ekf.py
@@ -8,7 +8,6 @@ from IPython.core.debugger import set_trace
 
 import utils
 
-
 class Ekf():
     def __init__(self, dyn_2d, model_sensor, prediction_jacobian, measurement_jacobian, sig_accel, sig_gyro, sig_gps):
         self.dyn_2d = dyn_2d
@@ -16,6 +15,8 @@ class Ekf():
         self.prediction_jacobian = prediction_jacobian
         self.measurement_jacobian = measurement_jacobian
         self.t_prev = 0
+        self.first=True
+        self.zt_prev = np.zeros((3,1))
         self.sig_accel = sig_accel
         self.sig_gyro = sig_gyro
         self.sig_gps = sig_gps
@@ -39,13 +40,18 @@ class Ekf():
 
     def measure(self, Mu_bar, Sig_bar, Zt):
         Ht= self.measurement_jacobian(Mu_bar, Zt)
+        if self.first:
+            self.zt_prev=Zt
+            self.zt_prev[2] = -np.pi/2
+        Zt[2]=-utils.wrap(np.arctan2(Zt[0]-self.zt_prev[0],Zt[1]-self.zt_prev[1])-np.pi/2)
+        self.zt_prev = Zt
         Qt = self.measurement_matrices()
-        St = Ht@Sig_bar[0:2,0:2]@Ht.T+Qt
-        Kt = Sig_bar[0:2,0:2]@Ht.T@inv(St)
-        # Kt = np.eye(2)
-        Mu_bar[0:2] = Mu_bar[0:2]+Kt@(Zt-Mu_bar[0:2])
+        St = Ht@Sig_bar@Ht.T+Qt
+        Kt = Sig_bar@Ht.T@inv(St)
+        z_hat = np.hstack([(Zt[0:2]-Mu_bar[0:2]).T, np.array([utils.wrap(Zt[2]-Mu_bar[2])]).T]).T
+        Mu_bar = Mu_bar+Kt@(z_hat)
         # set_trace()
-        Sig_bar[0:2,0:2] = (np.eye(2)-Kt@Ht)@Sig_bar[0:2,0:2]
+        Sig_bar = (np.eye(3)-Kt@Ht)@Sig_bar
 
         Sig = Sig_bar
         Mu = Mu_bar
@@ -68,7 +74,8 @@ class Ekf():
 
     def measurement_matrices(self):
 
-        Qt = np.array([[self.sig_gps**2, 0.0],\
-                       [0.0, self.sig_gps**2]])
+        # Qt = np.array([[self.sig_gps**2, 0.0],\
+        #                [0.0, self.sig_gps**2]])
+        Qt = np.diag([self.sig_gps**2, self.sig_gps**2, self.sig_gps**2])
 
         return Qt
